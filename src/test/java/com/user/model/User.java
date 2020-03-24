3f956007ca08a07f1aa33938ac8017937234d9d1
sevilayagil <sevilayagil@gmail.com>
2020-03-24 23:09:09 +0300

    New post  method added and user class updated

--- src/test/java/com/user/model/User.java
+++ src/test/java/com/user/model/User.java
@@ -1,8 +1,28 @@
-package user.model;
+package com.user.model;
 
 public class User {
     private String name;
     private String job;
+    private String emailFail;
+    private String emailSuccess;
+    private String password;
+    ;
+
+    public String getEmailSuccess() {
+        return emailSuccess;
+    }
+
+    public void setEmailSuccess(String emailSuccess) {
+        this.emailSuccess = emailSuccess;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public String getName() {
         return name;
@@ -19,4 +39,13 @@ public class User {
     public void setJob(String job) {
         this.job = job;
     }
+
+    public String getEmailFail() {
+        return emailFail;
+    }
+
+    public void setEmailFail(String email) {
+        this.emailFail = emailFail;
+    }
+
 }
--- src/test/java/com/user/tests/UserPostTest.java
+++ src/test/java/com/user/tests/UserPostTest.java
@@ -1,11 +1,15 @@
-package user.tests;
+package com.user.tests;
 
 import io.qameta.allure.*;
 import io.qameta.allure.junit4.DisplayName;
 import io.restassured.http.ContentType;
 import org.junit.Test;
-import user.base.TestBase;
-import user.model.User;
+import com.user.base.TestBase;
+import com.user.model.User;
+
+import static io.restassured.RestAssured.responseSpecification;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertEquals;
 
 import static io.restassured.RestAssured.given;
 
@@ -35,4 +39,23 @@ public class UserPostTest extends TestBase {
                         response().asString();
         System.out.println("Response is\t" + Resp);
     }
+
+    @DisplayName("Register User Unsuccessfully")
+    @Story("User is registering")
+    @Description("This request includes missing register for user")
+    @Test
+    public void registerUnsuccessful() {
+        User user = new User();
+        user.setEmailFail("sydney@fife");
+        given()
+                .contentType(ContentType.JSON)
+                .when()
+                .body(user)
+                .post("/register")
+                .then()
+                .statusCode(400)
+                .assertThat()
+                .body("error", equalTo("Missing email or username"));
+    }
+
 }
